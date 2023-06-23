636d990c96d9c0f298c37f5711c5f9c0993934c7
as-foxton <130701107+as-foxton@users.noreply.github.com>
2023-06-23 11:22:35 +0200

    First commit YC

---
+++ src/main/java/nl/yc2306/recruitmentApp/RecruitmentAppApplication.java
@@ -0,0 +1,13 @@
+package nl.yc2306.recruitmentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/yc2306/recruitmentApp/RecruitmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.yc2306.recruitmentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

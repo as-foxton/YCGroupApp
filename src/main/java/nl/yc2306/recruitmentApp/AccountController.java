26dd6f950a5945cc35ed21cd66f14746a606493d
Christel <christel.vanrhenen@gmail.com>
2023-06-27 10:01:11 +0200

    commit 27-6-2023

--- src/main/java/nl/yc2306/recruitmentApp/AccountController.java
+++ src/main/java/nl/yc2306/recruitmentApp/AccountController.java
@@ -3,6 +3,7 @@ package nl.yc2306.recruitmentApp;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(maxAge=3600)
 public class AccountController {
 	@Autowired
 	private AccountService service;

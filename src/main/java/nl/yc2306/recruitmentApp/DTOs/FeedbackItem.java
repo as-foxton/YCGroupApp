a0d8b6e91d9c1177cd218a4ec1deae5b8113274c
minh luu <minh_tech@hotmail.com>
2023-07-19 09:48:55 +0200

    reworked to work with dto FeedbackItem

---
+++ src/main/java/nl/yc2306/recruitmentApp/DTOs/FeedbackItem.java
@@ -0,0 +1,42 @@
+package nl.yc2306.recruitmentApp.DTOs;
+
+public class FeedbackItem {
+	private long id;
+	private String accountName;
+	private String bedrijf;
+	private String mening;
+	private boolean aangenomen;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public String getBedrijf() {
+		return bedrijf;
+	}
+	public void setBedrijf(String bedrijf) {
+		this.bedrijf = bedrijf;
+	}
+	public String getMening() {
+		return mening;
+	}
+	public void setMening(String mening) {
+		this.mening = mening;
+	}
+	
+	public boolean isAangenomen() {
+		return aangenomen;
+	}
+	public void setAangenomen(boolean aangenomen) {
+		this.aangenomen = aangenomen;
+	}
+}
